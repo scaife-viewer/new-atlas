07fe07df50ac5fc0e266e6aa0aa738d6d3697a08
James Tauber <jtauber@jtauber.com>
2024-10-29 07:14:49 -0400

    better count display on home page

--- server/atlas/views.py
+++ server/atlas/views.py
@@ -1,11 +1,11 @@
 from django.views.generic import TemplateView
 
-from .alignments.models import TextAlignment
-from .annotations.models import TextAnnotationCollection
+from .alignments.models import TextAlignment, TextAlignmentRecord
+from .annotations.models import TextAnnotationCollection, TextAnnotation
 from .attributions.models import AttributionPerson, AttributionOrganization, AttributionRecord
 from .audio_annotations.models import AudioAnnotation
 from .ctslibrary.precomputed import library_view_json
-from .dictionaries.models import Dictionary
+from .dictionaries.models import Dictionary, DictionaryEntry
 from .image_annotations.models import ImageAnnotation
 from .metrical_annotations.models import MetricalAnnotation
 from .morphology.models import Form, Lemma
@@ -23,9 +23,15 @@ class HomePageView(TemplateView):
 
             "tokens": Token.objects.count(),
             "nodes": Node.objects.count(),
+
             "dictionaries": Dictionary.objects.count(),
+            "dictionary_entries": DictionaryEntry.objects.count(),
+
             "text_annotation_collections": TextAnnotationCollection.objects.count(),
+            "text_annotations": TextAnnotation.objects.count(),
+
             "text_alignments": TextAlignment.objects.count(),
+            "text_alignment_records": TextAlignmentRecord.objects.count(),
 
             "attribution_people": AttributionPerson.objects.count(),
             "attribution_organizations": AttributionOrganization.objects.count(),
