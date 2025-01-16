9d33b0350dfccf6f17a39f204362184b808abe23
andrmayo <acmayo399@gmail.com>
2025-01-16 16:42:53 -0500

    Added startup script that can be run to get ATLAS running locally. Edited README.md to include instructions for this.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Aligned Text and Linguistic Annotation Server
 This code is under heavy development and is a step towards one component of Perseus 6.
 
 It is a consolidation and refactoring of backend code from Scaife 1.0 and the Beyond Translation project and will provide the data and services for the reading environment. However, ability to browse the data and annotations is supported and some features of the reading environment are being prototyped here.
+
+To get ATLAS running locally, first run "./startup.sh" to create a sqlite3 database ("/new-atlas/server/db/default.sqlite3") and necessary directories, as well as creating and running Django migrations. If you have files to ingest (e.g. in a directory "new-atlas/test-data/"), instead run "./startup.sh -i". If you've already run "./startup.sh" and want to ingest files, run "./startup.sh -n -i" to skip initial setup.
---
+++ startup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# A script to set up sqlite3 database and necessary directories
+# for manage.py runserver to work, and optionally to do ingestion of texts.
+
+SETUP=1
+INGEST=0
+
+while [ True ]; do
+  if [ "$1" = "--ingest" -o "$1" = "-i" ]; then
+    INGEST=1
+    shift 1
+  elif [ "$1" = "no-setup" -o "$1" = "-n" ]; then
+    SETUP=0
+    shift 1
+  else
+    break
+  fi
+done
+
+if [ $SETUP = 1 ]; then
+  cd server
+  mkdir -p ./data/cts
+  touch ./data/cts/README.md
+  mkdir ./db
+  sqlite3 ./db/default.sqlite3 ""
+  ./manage.py makemigrations
+  ./manage.py migrate
+fi
+
+if [ $INGEST = 1 ]; then
+  if [ $SETUP = 0 ]; then
+    cd server
+  fi
+  ./manage.py shell -c "from atlas.texts.ingestion import ingest_texts; ingest_texts(reset=True)"
+  ./manage.py shell -c "from atlas.texts.ingestion import tokenize_all_text_parts; tokenize_all_text_parts(reset=True)"
+  ./manage.py shell -c "from atlas.dictionaries.ingestion import ingest_dictionaries;  ingest_dictionaries(reset=True)"
+fi
