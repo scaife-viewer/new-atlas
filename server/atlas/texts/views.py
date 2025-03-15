d9d38bbd03888495abdf0614e118af78ea0b37ca
James Tauber <jtauber@jtauber.com>
2025-03-15 18:02:47 -0400

    show citations

--- server/atlas/texts/views.py
+++ server/atlas/texts/views.py
@@ -1,3 +1,5 @@
+from django.apps import apps
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 
 from .models import Node
@@ -13,3 +15,10 @@ class NodeDetailView(DetailView):
     slug_field = "urn"
     slug_url_kwarg = "urn"
     model = Node
+
+    def citations(self):
+        urn = self.object.urn
+        # urn.version = None
+        Citation = apps.get_model("dictionaries.Citation")
+        citations = Citation.objects.filter(Q(data__urn=str(urn))|Q(data__urn=str(self.object.urn)))
+        return str(urn), citations
