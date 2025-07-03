04d7ca504d5d501579d8fe0379750bf03974291d
James Tauber <jtauber@jtauber.com>
2025-07-03 09:27:44 -0400

    remove Latin coming soon

--- server/ingest.sh
+++ server/ingest.sh
@@ -1,6 +1,6 @@
 # ./manage.py shell -c "from atlas.texts.ingestion import ingest_texts; ingest_texts(reset=True)"
 # ./manage.py shell -c "from atlas.texts.ingestion import tokenize_all_text_parts; tokenize_all_text_parts(reset=True)"
-# ./manage.py shell -c "from atlas.dictionaries.ingestion import ingest_dictionaries; ingest_dictionaries(reset=True)"
+./manage.py shell -c "from atlas.dictionaries.ingestion import ingest_dictionaries; ingest_dictionaries(reset=True)"
 # ./manage.py shell -c "from atlas.alignments.ingestion import ingest_alignments; ingest_alignments(reset=True)"
 # ./manage.py shell -c "from atlas.annotations.ingestion import ingest_syntaxtrees; ingest_syntaxtrees(reset=True)"
 # ./manage.py shell -c "from atlas.morphology.models import import_data; import_data('../test-data/glaux_morph_counts2.tsv', 'grc')"
@@ -12,4 +12,6 @@
 # ./manage.py shell -c "from atlas.audio_annotations.ingestion import ingest_audio_annotations; ingest_audio_annotations(reset=True)"
 # ./manage.py shell -c "from atlas.image_annotations.ingestion import ingest_image_annotations; ingest_image_annotations(reset=True)"
 
-./manage.py shell -c "from atlas.commentaries.ingestion import ingest_commentaries; ingest_commentaries(reset=True)"
+# ./manage.py shell -c "from atlas.commentaries.ingestion import ingest_commentaries; ingest_commentaries(reset=True)"
+
+# ./manage.py shell -c "from atlas.morphology.models import import_data; import_data('../test-data/morphology/ola_morph_counts.tsv', 'lat')"
